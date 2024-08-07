9b5ef628cb87323cb03650ec0a82167a524dc4c9
ww-ahyuda <wahyuda542@smk.belajar.id>
2024-08-07 14:04:49 +0700

    1. Buatlah variabel menggunakan let dan const

---
+++ tugas.js
@@ -0,0 +1,14 @@
+//1. Buatlah variabel menggunakan let dan const
+// test
+// Menggunakan let
+let nama = "Andra";
+console.log(nama); // Output: Andra
+
+nama = "Bang Andra";
+console.log(nama); // Output: Bapak Andra
+
+// Menggunakan const
+const pi = 3.14;
+console.log(pi); // Output: 3.14
+// pi = 3.14159; // Error: Assignment to constant variable
+
