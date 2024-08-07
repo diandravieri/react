5add0d02934bcc3412d320000c5cf183b723f0cf
ww-ahyuda <wahyuda542@smk.belajar.id>
2024-08-07 14:06:33 +0700

    2. Buat arrow function yang menerima parameter dan mengembalikan nilai

--- tugas.js
+++ tugas.js
@@ -1,5 +1,4 @@
 //1. Buatlah variabel menggunakan let dan const
-// test
 // Menggunakan let
 let nama = "Andra";
 console.log(nama); // Output: Andra
@@ -12,3 +11,6 @@ const pi = 3.14;
 console.log(pi); // Output: 3.14
 // pi = 3.14159; // Error: Assignment to constant variable
 
+//2. Buat arrow function yang menerima parameter dan mengembalikan nilai
+const sapa = (nama) => `Halo, ${nama}!`;
+console.log(sapa("Andra")); // Output: Halo, Andra!
